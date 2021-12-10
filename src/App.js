f2308a1d3099e4ee9fc3ec3b65a39a6d3d572a2e
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2021-12-10 10:34:29 -0500

    added api call to app.js and set characters to characters arr from api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +9,24 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  // set state to get and set characters
+  const [chars, setChars] = useState([])
+
+  // make a (mock) api call to get characters data from link, then set characters to characters list
+  useEffect(() => {
+    const fetchChar = () => {
+      axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        console.log('data:', res.data);
+        setChars(res.data);
+      })
+      .catch(err => {
+        debugger
+      })
+    }
+    fetchChar();
+  }, [])
 
   return (
     <div className="App">
