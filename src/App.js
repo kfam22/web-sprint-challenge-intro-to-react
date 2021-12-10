79dc47752cc3b7d64466d2869938aa59b3d628fa
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2021-12-10 13:39:31 -0500

    updated id/key to idx

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import Character from './components/Character';
-import uuid from 'uuid/dist/v4';
-
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -14,14 +12,14 @@ const App = () => {
   // sync up with, if any.
 
   // set state to get and set characters
-  const [chars, setChars] = useState([])
+  const [chars, setChars] = useState([]);
 
   // make a (mock) api call to get characters data from link, then set characters to characters list
   useEffect(() => {
     const fetchChar = () => {
       axios.get('https://swapi.dev/api/people')
       .then(res => {
-        console.log('data:', res.data[0].name);
+        console.log('data:', res.data);
         setChars(res.data);
       })
       .catch(err => {
@@ -36,12 +34,11 @@ const App = () => {
     <div className="App">
       <h1 className="Header">React Wars</h1>
       {
-        chars.map(char => {
-          const charId = uuid();
+        chars.map((char, idx) => {
           return <Character
           info={char}
-          key={charId}
-          id={charId}
+          key={idx}
+          id={idx}
           />
         })}
     </div>
