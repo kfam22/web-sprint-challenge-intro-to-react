a011908bc78b019dc501b7548223f4fdac939785
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2021-12-10 11:42:55 -0500

    rendered character component to the DOM

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import Character from './components/Character';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -18,7 +20,7 @@ const App = () => {
     const fetchChar = () => {
       axios.get('https://swapi.dev/api/people')
       .then(res => {
-        console.log('data:', res.data);
+        console.log('data:', res.data[0].name);
         setChars(res.data);
       })
       .catch(err => {
@@ -30,7 +32,13 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">React Wars</h1>
+      {
+        chars.map(char => {
+          return <Character
+          info={char}
+          />
+        })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React from 'react';
+
+
+export default function Character(props) {
+    const { info } = props;
+    return (
+        <div>
+            <h2>{info.name}</h2>
+        </div>
+    )
+}
